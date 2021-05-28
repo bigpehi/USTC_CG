6fea306c1f001700f64026406d926e9ed9f8f288
Chambin <2281181505@qq.com>
2021-05-28 15:41:45 +0800

    homework 0-1

--- Homeworks/0_CppPratices/project/src/executables/1_BasicDArray/DArray.cpp
+++ Homeworks/0_CppPratices/project/src/executables/1_BasicDArray/DArray.cpp
@@ -1,87 +1,144 @@
 // implementation of class DArray
+#include<iostream>
+#include <cassert>
 #include "DArray.h"
-
+using namespace std;
 // default constructor
 DArray::DArray() {
+	//默认构造函数
 	Init();
 }
 
 // set an array with default values
-DArray::DArray(int nSize, double dValue) {
-	//TODO
+DArray::DArray(int nSize, double dValue):m_pData(new double[nSize]),m_nSize(nSize){
+	//有参构造函数
+	for (int i = 0; i < nSize; i++) {
+		m_pData[i] = dValue;
+	}
+	
 }
 
-DArray::DArray(const DArray& arr) {
-	//TODO
+DArray::DArray(const DArray& arr):m_pData(new double[arr.m_nSize]),m_nSize(arr.m_nSize){
+	//拷贝构造函数
+	for (int i = 0; i < m_nSize; i++) {
+		m_pData[i] = arr.m_pData[i];
+	}
 }
 
 // deconstructor
 DArray::~DArray() {
+	//析构函数
 	Free();
 }
 
 // display the elements of the array
 void DArray::Print() const {
-	//TODO
+	cout << "size = " << m_nSize << ":";
+	for (int i = 0; i < m_nSize; i++) {
+		cout << m_pData[i] << "	";
+	}
+	cout << endl;
 }
 
 // initilize the array
 void DArray::Init() {
-	//TODO
+	m_nSize = 0;
+	m_pData = nullptr;
 }
 
 // free the array
 void DArray::Free() {
-	//TODO
+	delete[] m_pData;
+	m_pData = nullptr;
+	m_nSize = 0;
 }
 
 // get the size of the array
 int DArray::GetSize() const {
-	//TODO
-	return 0; // you should return a correct value
+	return m_nSize;
 }
 
 // set the size of the array
-void DArray::SetSize(int nSize) {
-	//TODO
+void DArray::SetSize(int nSize){
+	double* m_pData_temp = new double[nSize];
+	if (nSize >= m_nSize) {
+		for (int i = 0; i < m_nSize; i++)
+			m_pData_temp[i] = m_pData[i];
+		for (int i = m_nSize; i < nSize; i++)
+			m_pData_temp[i] = 0.;
+	}
+	else {
+		for (int i = 0; i < nSize; i++)
+			m_pData_temp[i] = m_pData[i];
+	}
+	delete[] m_pData; // star 忘记释放
+	m_pData = m_pData_temp;
+	m_nSize = nSize;
 }
 
 // get an element at an index
 const double& DArray::GetAt(int nIndex) const {
-	//TODO
-	static double ERROR; // you should delete this line
-	return ERROR; // you should return a correct value
+	assert(nIndex >= 0 && nIndex < m_nSize); //star 断言
+	return m_pData[nIndex];
 }
 
 // set the value of an element 
 void DArray::SetAt(int nIndex, double dValue) {
-	//TODO
+	assert(nIndex >= 0 && nIndex < m_nSize);
+	m_pData[m_nSize] = dValue;
 }
 
 // overload operator '[]'
 const double& DArray::operator[](int nIndex) const {
-	//TODO
-	static double ERROR; // you should delete this line
-	return ERROR; // you should return a correct value
+	assert(nIndex >= 0 && nIndex < m_nSize);
+	return m_pData[nIndex];
 }
 
 // add a new element at the end of the array
 void DArray::PushBack(double dValue) {
-	//TODO
+	double* m_pData_temp = new double[m_nSize + 1];
+	for (int i = 0; i < m_nSize; i++)
+		m_pData_temp[i] = m_pData[i];
+	m_pData_temp[m_nSize] = dValue;
+	m_nSize++;
+	delete[] m_pData;
+	m_pData = m_pData_temp;
 }
 
 // delete an element at some index
 void DArray::DeleteAt(int nIndex) {
-	//TODO
+	assert(nIndex >= 0 && nIndex < m_nSize);
+	double* m_pData_temp = new double[m_nSize - 1];
+	for (int i = 0; i < nIndex; i++)
+		m_pData_temp[i] = m_pData[i];
+	for (int i = nIndex; i+1 < m_nSize; i++)
+		m_pData_temp[i] = m_pData[i+1];
+	m_nSize--;
+	delete[] m_pData;
+	m_pData = m_pData_temp;
 }
 
 // insert a new element at some index
 void DArray::InsertAt(int nIndex, double dValue) {
-	//TODO
+	assert(nIndex >= 0 && nIndex <= m_nSize);
+	double* m_pData_temp = new double[m_nSize + 1];
+	for (int i = 0; i < nIndex; i++)
+		m_pData_temp[i] = m_pData[i];
+	m_pData_temp[nIndex] = dValue;
+	for (int i = nIndex; i < m_nSize; i++)
+		m_pData_temp[i + 1] = m_pData[i];
+	m_nSize++;
+	delete[] m_pData;
+	m_pData = m_pData_temp;
 }
 
-// overload operator '='
+// overload operator '=' , arr is the right value.
 DArray& DArray::operator = (const DArray& arr) {
-	//TODO
+	delete[] m_pData;
+	m_pData = new double[arr.m_nSize];
+	for (int i = 0; i < arr.m_nSize; i++) {
+		m_pData[i] = arr[i];
+	}
+	m_nSize = arr.m_nSize;
 	return *this;
 }
